da56c06cbc68cf31bd3239fe6c953c67f34d9d2f
badrAZ <azizbibadr@gmail.com>
2019-05-21 15:27:35 +0200

    Backup ng API & UI

--- packages/xo-web/src/xo-app/backup-ng/new/index.js
+++ packages/xo-web/src/xo-app/backup-ng/new/index.js
@@ -22,7 +22,7 @@ import { injectState, provideState } from 'reaclette'
 import { Map } from 'immutable'
 import { Number } from 'form'
 import { renderXoItemFromId, Remote } from 'render-xo-item'
-import { SelectRemote, SelectSr, SelectVm } from 'select-objects'
+import { SelectProxy, SelectRemote, SelectSr, SelectVm } from 'select-objects'
 import {
   addSubscriptions,
   connectStore,
@@ -122,6 +122,7 @@ const createDoesRetentionExist = name => {
 
 const getInitialState = () => ({
   _displayAdvancedSettings: undefined,
+  _proxyId: undefined,
   _vmsPattern: undefined,
   backupMode: false,
   compression: undefined,
@@ -183,9 +184,10 @@ export default decorate([
         }
 
         await createBackupNgJob({
-          name: state.name,
-          mode: state.isDelta ? 'delta' : 'full',
           compression: state.compression,
+          mode: state.isDelta ? 'delta' : 'full',
+          name: state.name,
+          proxyId: state.proxyId === null ? undefined : state.proxyId,
           schedules: mapValues(
             state.schedules,
             ({ id, ...schedule }) => schedule
@@ -264,10 +266,11 @@ export default decorate([
         )
 
         await editBackupNgJob({
-          id: props.job.id,
-          name: state.name,
-          mode: state.isDelta ? 'delta' : 'full',
           compression: state.compression,
+          id: props.job.id,
+          mode: state.isDelta ? 'delta' : 'full',
+          name: state.name,
+          proxyId: state.proxyId,
           settings: normalizeSettings({
             settings: settings || state.propSettings,
             exportMode: state.exportMode,
@@ -473,6 +476,9 @@ export default decorate([
         return getInitialState()
       },
       setCompression: (_, compression) => ({ compression }),
+      setProxy(_, proxy) {
+        this.state._proxyId = resolveId(proxy)
+      },
       toggleDisplayAdvancedSettings: () => ({ displayAdvancedSettings }) => ({
         _displayAdvancedSettings: !displayAdvancedSettings,
       }),
@@ -524,8 +530,17 @@ export default decorate([
       formId: generateId,
       inputConcurrencyId: generateId,
       inputFullIntervalId: generateId,
+      inputProxyId: generateId,
       inputTimeoutId: generateId,
 
+      disabledCrMode: state =>
+        state.proxyId != null ||
+        state.isFull ||
+        (!state.crMode && process.env.XOA_PLAN < 4),
+      disabledDrMode: state =>
+        state.proxyId != null ||
+        state.isDelta ||
+        (!state.drMode && process.env.XOA_PLAN < 3),
       vmsPattern: ({ _vmsPattern }, { job }) =>
         defined(
           _vmsPattern,
@@ -604,6 +619,7 @@ export default decorate([
               }
             : setting
         ),
+      proxyId: ({ _proxyId }, { job }) => defined(_proxyId, () => job.proxyId),
       displayAdvancedSettings: (state, props) =>
         defined(
           state._displayAdvancedSettings,
@@ -689,10 +705,7 @@ export default decorate([
                     <ActionButton
                       active={state.drMode}
                       data-mode='drMode'
-                      disabled={
-                        state.isDelta ||
-                        (!state.drMode && process.env.XOA_PLAN < 3)
-                      }
+                      disabled={state.disabledDrMode}
                       handler={effects.toggleMode}
                       icon='disaster-recovery'
                     >
@@ -706,10 +719,7 @@ export default decorate([
                     <ActionButton
                       active={state.crMode}
                       data-mode='crMode'
-                      disabled={
-                        state.isFull ||
-                        (!state.crMode && process.env.XOA_PLAN < 4)
-                      }
+                      disabled={state.disabledCrMode}
                       handler={effects.toggleMode}
                       icon='continuous-replication'
                     >
@@ -866,6 +876,16 @@ export default decorate([
                   </ActionButton>
                 </CardHeader>
                 <CardBlock>
+                  <FormGroup>
+                    <label htmlFor={state.inputProxyId}>
+                      <strong>{_('proxy')}</strong>
+                    </label>
+                    <SelectProxy
+                      id={state.inputProxyId}
+                      onChange={effects.setProxy}
+                      value={state.proxyId}
+                    />
+                  </FormGroup>
                   <ReportWhen
                     onChange={effects.setReportWhen}
                     required
