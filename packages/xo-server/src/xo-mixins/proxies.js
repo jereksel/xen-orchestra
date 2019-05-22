e6b40d7e1554ef9319e25015b825b6c57393b1a0
badrAZ <azizbibadr@gmail.com>
2019-05-22 14:50:51 +0200

    feat(proxy): ability to run a backup throught a proxy
    Fixes #1201
    
    [xo-proxy specification](https://github.com/vatesfr/xen-orchestra/blob/xo-proxy/@xen-orchestra/proxy/docs/api.md)
    
    - [ ] proxy API (b8c16a97b0d9ee80c9bea41bb023e766f84c9ac4)
    - [ ] update remote create/edit API and UI (c72084227afc63d99b9b895e28dda8ada8e0dbb5)
    - [ ] update backup NG create/edit API and UI (f75ecb97355c356723694b7edf753c896774224f)
    - [ ] update metadata backup create/edit API and UI (653477b17078e7efc1773bee8d6df1ceece32648)
    - [ ] ensure all remote API methods are working via the proxy

---
+++ packages/xo-server/src/xo-mixins/proxies.js
@@ -0,0 +1,49 @@
+import { noSuchObject } from 'xo-common/api-errors'
+
+import Collection from '../collection/redis'
+import patch from '../patch'
+import { mapToArray } from '../utils'
+
+export default class Proxy {
+  constructor(app) {
+    const db = (this._db = new Collection({
+      connection: app._redis,
+      prefix: 'xo:proxy',
+    }))
+
+    app.on('clean', () => db.rebuildIndexes())
+    app.on('start', () =>
+      app.addConfigManager(
+        'proxies',
+        () => db.get(),
+        proxies => Promise.all(mapToArray(proxies, proxy => db.update(proxy)))
+      )
+    )
+  }
+
+  registerProxyAppliance(appliance) {
+    return this._db.add(appliance).then(({ properties }) => properties)
+  }
+
+  unRegisterProxyAppliance(id) {
+    return this._db.remove(id)
+  }
+
+  async getProxy(id) {
+    const proxy = await this._db.first(id)
+    if (proxy === undefined) {
+      throw noSuchObject(id, 'proxy')
+    }
+    return proxy.properties
+  }
+
+  getAllProxies() {
+    return this._db.get()
+  }
+
+  async updateProxy(id, props) {
+    const proxy = await this.getProxy(id)
+    patch(proxy, props)
+    return this._db.update(proxy).then(({ properties }) => properties)
+  }
+}
